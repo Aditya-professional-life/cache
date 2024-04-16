23d818a30e6bc1912d2f3926f8eadf47e3abd3c1
Aditya <adi.professional.life@gmail.com>
2024-04-16 13:22:22 +0530

    thibk

--- Minimize_the_Difference.py
+++ Minimize_the_Difference.py
@@ -1,48 +1,17 @@
-import heapq as hq
 from typing import List
 
-
 class Solution:
-    def minimizeDifference(self,n,k,arr):
-        dic = {}
-        for i in arr:
-            if i in dic:
-                dic[i]+=1
-            else:
-                dic[i]=1
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # max1 = max2 = float('-inf')
-        # for i in arr:
-        #     if i > max1:
-        #         max2 = max1
-        #         max1 = i
-        #     elif max1 > i >max2:
-        #         max2 = i
-
-        # return (max1 - max2)
-
+    def minimizeDifference(self, n: int, k: int, arr: List[int]) -> int:
+        arr.sort()
+        min_diff = float('inf')
+        for i in range(n - k + 1):
+            min_diff = min(min_diff, arr[i + k - 1] - arr[i])
+        return min_diff
 
 s1 = Solution()
-n = 6
+
 k = 3
 arr = [2, 3, 1, 4, 6, 7]
-result = s1.minimizeDifference(n,k,arr)
+n = len(arr)
+result = s1.minimizeDifference(n, k, arr)
 print(result)
